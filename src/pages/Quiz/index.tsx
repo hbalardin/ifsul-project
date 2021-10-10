2768b5fa9406528f652e64d9849c30381b04f8b1
Henrique Balardin <balardinh@gmail.com>
2021-10-10 01:27:08 -0300

    feature (quiz): update payload to new structure

--- src/pages/Quiz/index.tsx
+++ src/pages/Quiz/index.tsx
@@ -1,63 +1,91 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import quiz from '../../mocks/quiz.json';
 
 interface Question {
+  id: string;
   title: string;
-  answers: string[];
-  linkedQuestions?: Question[]
+}
+
+interface Answer {
+  id: string;
+  title: string;
+  description: string;
+  questionId: string
+  nextQuestionId?: string;
 }
 
 export const Quiz = (): JSX.Element => {
-  const [currentQuestion, setCurrentQuestion] = useState<Question>(quiz.data);
-  const [results, setResults] = useState('');
+  const [currentQuestion, setCurrentQuestion] = useState<Question>();
+  const [currentAnswers, setCurrentAnswers] = useState<Answer[]>([]);
+  const [showResults, setShowResults] = useState(false);
 
-  const handleFinish = (): void => {
-    setResults('Congratulations!');
+  const startQuiz = (): void => {
+    const firstQuestion = quiz.data.questions[0];
+    const firstAnswers = quiz.data.answers.filter((answer) => answer.questionId === firstQuestion.id);
+
+    setCurrentQuestion(firstQuestion);
+    setCurrentAnswers(firstAnswers);
+    setShowResults(false);
   };
 
-  const handleSelectAnswer = (index: number): void => {
-    if (currentQuestion?.linkedQuestions) {
-      return setCurrentQuestion(currentQuestion.linkedQuestions[index]);
+  const handleSelectAnswer = (nextQuestionId?: string): void => {
+    if (!nextQuestionId) {
+      setShowResults(true);
+      return;
     }
 
-    return handleFinish();
+    const nextQuestion = quiz.data.questions.find((question) => question.id === nextQuestionId);
+    const nextAnswers = quiz.data.answers.filter((answer) => answer.questionId === nextQuestionId);
+
+    setCurrentQuestion(nextQuestion);
+    setCurrentAnswers(nextAnswers);
   };
 
-  const handleRestart = (): void => {
-    setCurrentQuestion(quiz.data);
-    setResults('');
-  };
+  useEffect(() => {
+    startQuiz();
+  }, []);
 
   return (
-    <div>
-      {results ? (
-        <>
-          <div>{results}</div>
-          <button type="button" onClick={handleRestart}>Restart</button>
-        </>
-      ) : (
-        <>
-          <h1>{currentQuestion.title}</h1>
-          {currentQuestion?.answers?.map((answer, index) => (
-            // just for now
-            /* eslint-disable-next-line */
-            <div
-              // just for now
-              /* eslint-disable-next-line */
-              key={index}
-              style={{
-                padding: '24px 32px',
-                border: '1px solid #000',
-                cursor: 'pointer',
-              }}
-              onClick={() => handleSelectAnswer(index)}
-            >
-              {answer}
+    <section>
+      {showResults
+        ? (
+          <>
+            <h1>Quiz finalizado!</h1>
+            <button onClick={startQuiz} type="button">Reiniciar</button>
+          </>
+        )
+        : (
+          <>
+            <h1>{currentQuestion?.title}</h1>
+            <div>
+              <ul style={{ padding: 0 }}>
+                {currentAnswers.map((answer) => (
+                  <li
+                    key={answer.id}
+                    style={{
+                      listStyle: 'none',
+                    }}
+                  >
+                    <button
+                      type="button"
+                      onClick={() => handleSelectAnswer(answer.nextQuestionId)}
+                      style={{
+                        padding: '24px 32px',
+                        border: '1px solid #000',
+                        cursor: 'pointer',
+                        width: '100%',
+                        textAlign: 'left',
+                      }}
+                    >
+                      {answer.title}
+                    </button>
+                  </li>
+                ))}
+              </ul>
             </div>
-          ))}
-        </>
-      )}
-    </div>
+          </>
+        )}
+    </section>
   );
 };
