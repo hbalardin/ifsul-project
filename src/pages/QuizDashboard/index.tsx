60d516b14ee408be7a950801e50df30c1dd31f09
Henrique Balardin <balardinh@gmail.com>
2021-11-03 01:27:33 -0300

    chore: create quiz dashboard page styles

--- src/pages/QuizDashboard/index.tsx
+++ src/pages/QuizDashboard/index.tsx
@@ -1,6 +1,70 @@
-export const QuizDashboard = (): JSX.Element => (
-  <main>
-    <h1>Gerencie o seu quiz:</h1>
+import { useEffect, useState } from 'react';
 
-  </main>
-);
+import quiz from '../../mocks/quiz.json';
+
+import { Answer, Question, StepProps } from '../../models';
+import {
+  AnswerEditableCard, AnswersSidebar, Button, EditableTitle, StepsNavbar,
+} from '../../components';
+
+import { Container, Content } from './styles';
+
+export const QuizDashboard = (): JSX.Element => {
+  const [currentQuestion, setCurrentQuestion] = useState<Question>();
+  const [currentAnswers, setCurrentAnswers] = useState<Answer[]>([]);
+  const [selectedAnswer, setSelectedAnswer] = useState<Answer>();
+
+  const [steps, setSteps] = useState<StepProps[]>([]);
+
+  const handleSelectAnswer = (answer: Answer): void => {
+    if (answer.id === selectedAnswer?.id) {
+      setSelectedAnswer(undefined);
+      return;
+    }
+
+    setSelectedAnswer(answer);
+  };
+
+  const fetchQuizData = (): void => {
+    const firstQuestion = quiz.data.questions[0];
+    const firstAnswers = quiz.data.answers.filter((answer) => answer.questionId === firstQuestion.id);
+
+    setCurrentQuestion(firstQuestion);
+    setCurrentAnswers(firstAnswers);
+
+    const firstSteps: StepProps[] = [
+      { id: '1', questionId: firstQuestion.id },
+    ];
+    setSteps(firstSteps);
+  };
+
+  useEffect(() => {
+    fetchQuizData();
+  }, []);
+
+  return (
+    <Container>
+      <AnswersSidebar showSidebar />
+      <Content>
+        <header>
+          <StepsNavbar steps={steps} />
+        </header>
+        <section>
+          {currentQuestion && <EditableTitle size={32} centered title={currentQuestion.title} onChangeTitle={(title: string) => setCurrentQuestion({ ...currentQuestion, title })} />}
+          <ul style={{ padding: 0 }}>
+            {currentAnswers.map((answer) => (
+              <AnswerEditableCard
+                key={answer.id}
+                title={answer.title}
+                isSelected={answer.id === selectedAnswer?.id}
+              />
+            ))}
+          </ul>
+        </section>
+        <footer>
+          <Button onClick={() => undefined}>Salvar</Button>
+        </footer>
+      </Content>
+    </Container>
+  );
+};
---
+++ src/pages/QuizDashboard/styles.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  min-height: 100vh;
+  min-width: 100vw;
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+`;
+
+export const Content = styled.article`
+  width: 100%;
+  padding: 24px 48px;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 80px;
+    width: 100%;
+  }
+
+  section {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    padding: 12px 0;
+
+    ul {
+      margin-top: 32px;
+
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      gap: 32px;
+    }
+  }
+
+  footer {
+    display: flex;
+    justify-content: center;
+
+    padding: 24px 0;
+  }
+`;
